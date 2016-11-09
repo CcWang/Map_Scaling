78e0e7a2766d640e80f8fad9b3f412d919edd2cd
CC Wang <chongchong1110@gmail.com>
2016-11-08 21:55:53 -0800

    bing map api

---
+++ front_end/myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AqwKmBTiuFl3Xj5OwqSHS7mjxUFE9mvHA2513saLgRwABPbuuzEULooiIbYgK2Y2'
+    });
+}
